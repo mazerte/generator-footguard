be40c1dde6a7a1fbf65836629a99b1567cd6f5c5
Mathieu Desvé <mathieu.desve@me.com>
2013-12-16 13:42:39 +0100

    Fix symbolic link directory

--- deploy_test.sh
+++ deploy_test.sh
@@ -4,6 +4,7 @@ then
     cd ..
     git clone https://github.com/mazerte/test-footguard.git && cd test-footguard
     rm -r ./* -f
+    mkdir node_modules
     ln -s ../generator-footguard node_modules/generator-footguard
     yo footguard
     git add --all && git commit -m "ref to build $TRAVIS_BUILD_ID"
