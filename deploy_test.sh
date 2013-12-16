7c37ec6c321d670a44d173d4bdab43beabcd0588
Mathieu Desvé <mathieu.desve@me.com>
2013-12-16 14:13:05 +0100

    Add ls for debuging

--- deploy_test.sh
+++ deploy_test.sh
@@ -5,13 +5,13 @@ then
     git clone https://github.com/mazerte/test-footguard.git && cd test-footguard
     rm -r ./* -f
     mkdir node_modules
-    ln -s ../generator-footguard node_modules/generator-footguard
+    ln -s node_modules/generator-footguard ../generator-footguard/
     npm install -g yo grunt-cli bower
     gem install compass
     ls -Flash
     ls -Flash node_modules/
     ls -Flash node_modules/generator-footguard
-    # yo footguard --no-insight
+    yo footguard --no-insight
     # git add --all && git commit -m "ref to build $TRAVIS_BUILD_ID"
 else
 	echo "Do nothing"
