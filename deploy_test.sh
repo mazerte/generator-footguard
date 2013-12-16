18ea5b940eb9941cbc2169df46a2d0e66626a7f9
Mathieu Desvé <mathieu.desve@me.com>
2013-12-16 13:33:28 +0100

    Add after success hook for deploy in test-fooguard project

---
+++ deploy_test.sh
@@ -0,0 +1,12 @@
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+	echo "Deploy test-footguard"
+    cd ..
+    git clone https://github.com/mazerte/test-footguard.git && cd test-footguard
+    rm -r ./* -f
+    ln -s ../generator-footguard node_modules/generator-footguard
+    yo footguard
+    git add --all && git commit -m "ref to build $TRAVIS_BUILD_ID"
+else
+	echo "Do nothing"
+fi
