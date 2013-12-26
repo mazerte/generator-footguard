d85d33a7b21de3fdc30de440a495a8e20ab76bdb
Mathieu Desvé <mathieu.desve@me.com>
2013-12-26 15:52:19 +0100

    Fix gh-pages deploy script

--- app/templates/scripts/gh-pages.sh
+++ app/templates/scripts/gh-pages.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-repo=$(git config --get remote.origin.url)
+if [ -z "$REPO" ]
+then
+	REPO=$(git config --get remote.origin.url)
+else
+	author=$(git log -1 --pretty=%an)
+	git config --global user.name "$author"
+	email=$(git log -1 --pretty=%ae)
+	git config --global user.email "$email"
+fi
 message=$(git log -1 --pretty=%B)
 
 cd dist
 rm -rf .git
 git init
-git remote add origin $repo
-git push origin --delete gh-pages
+git remote add origin $REPO > /dev/null 2>&1
+git push origin --delete gh-pages > /dev/null 2>&1
 git checkout -b gh-pages
 git add --all
 git commit -m "$message"
-git push origin gh-pages
+git push origin gh-pages > /dev/null 2>&1
